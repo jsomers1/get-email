50f281e75013da5deb199e48175023221b92352e
JS <jsomers1@protonmail.com>
2019-07-26 21:50:28 -0500

    changes

--- get_email.rb
+++ get_email.rb
@@ -1,1 +1,16 @@
 puts "here"
+
+
+
+Cross-platform solution:
+First, install the Launchy gem:
+
+$ gem install launchy
+Then, you can run this:
+
+require 'launchy'
+
+Launchy.open("http://stackoverflow.com")
+
+
+system("firefox http://www.google.com")
