a426b7c4720dfce19caeb5c899a4266126031dd9
JS <jsomers1@protonmail.com>
2019-08-04 23:15:21 -0500

    working on .py

--- get_email.py
+++ get_email.py
@@ -1,27 +1,50 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import time
 
-
-
-driver = webdriver.Firefox(...)  # Or Chrome(), or Ie(), or Opera()
-
-username = driver.find_element_by_id("username")
-password = driver.find_element_by_id("password")
-
-username.send_keys("YourUsername")
-password.send_keys("Pa55worD")
-
-driver.find_element_by_name("submit").click()
+# options = webdriver.ChromeOptions()
+# options.add_argument('--ignore-certificate-errors')
+# options.add_argument("--test-type")
+# options.binary_location = "/usr/bin/chromium"
+# driver = webdriver.Chrome(chrome_options=options)
+driver = webdriver.Chrome()
+driver.get('https://python.org')
 
 
 
-browser = webdriver.Firefox()
-browser.get("https://lbjcrs.ust.hk/primo/authen.php") 
-time.sleep(10)
-username = browser.find_element_by_id("extpatid")
-password = browser.find_element_by_id("extpatpw")
-username.send_keys("username")
-password.send_keys("password")
-login_attempt = browser.find_element_by_xpath("//*[@type='submit']")
-login_attempt.submit()
+# from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+# import time
+
+
+
+# driver = webdriver.Chrome(...)  # Or Chrome(), or Ie(), or Opera()
+
+# username = driver.find_element_by_id("username")
+# password = driver.find_element_by_id("password")
+
+# username.send_keys("YourUsername")
+# password.send_keys("Pa55worD")
+
+# driver.find_element_by_name("submit").click()
+
+
+
+# username = browser.find_element_by_name('username')
+# username.send_keys('user1')
+
+# password = browser.find_element_by_name('password')
+# password.send_keys('secret')
+
+# form = browser.find_element_by_id('loginForm')
+# form.submit()
+
+
+# browser = webdriver.Firefox()
+# browser.get("https://lbjcrs.ust.hk/primo/authen.php") 
+# time.sleep(10)
+# username = browser.find_element_by_id("extpatid")
+# password = browser.find_element_by_id("extpatpw")
+# username.send_keys("username")
+# password.send_keys("password")
+# login_attempt = browser.find_element_by_xpath("//*[@type='submit']")
+# login_attempt.submit()
